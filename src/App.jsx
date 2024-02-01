0286a5cf2434946aa1cf1235e6a02d82a4523239
Shivam Shende <Shivamshende200@gmail.com>
2024-02-01 21:39:24 +0530

    fix - Add index route for home page

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ function App() {
         <Route path='about' element={<About />} />
         {/* <Route path='github' element={<Github />} /> */}
 
+        <Route index element={<Home />} />
+
         <Route path='*' element={<NoMatch />} />
       </Routes>
     </>
